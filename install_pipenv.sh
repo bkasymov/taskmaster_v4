ead4a3ac18fbbb563712d5f8b832ff15ee372810
dpenney <dpenney@student.42.fr>
2024-08-06 15:32:41 +0200

    add script install pipenv

--- install_pipenv.sh
+++ install_pipenv.sh
@@ -1,41 +1,32 @@
 #!/bin/bash
 
-# Set up environment variables
+sudo apt-get update
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
+
+curl https://pyenv.run | bash
+
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-source ~/.bashrc
 
-# Install pyenv dependencies
-sudo apt-get update
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
 
-# Install pyenv
-curl https://pyenv.run | bash
-
-# Reload shell
-exec $SHELL
-
-# Install Python 3.11.7
 pyenv install 3.11.7
-
-# Set Python 3.11.7 as the global version
 pyenv global 3.11.7
 
-# Verify Python version
 python --version
 
-# Install pipenv
 pip install --user pipenv
 
-# Add pipenv to PATH
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+export PATH="$HOME/.local/bin:$PATH"
 
-# Verify pipenv installation
 pipenv --version
 
-echo "Python 3.11.7 and pipenv have been installed successfully!"
+echo "pyenv, Python 3.11.7 и pipenv успешно установлены!"
